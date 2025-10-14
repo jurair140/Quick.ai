b05ebdf7e4a8dd3e7dd26005c025f8b7da267085
jurair <jurair140@gmail.com>
2025-10-15 01:06:42 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import Community from "./pages/Community"
+import Layout from "./pages/Layout"
+import WriteArticle from "./pages/WriteArticle"
+import BlogTitle from "./pages/BlogTitle"
+import GenerateImages from "./pages/GenerateImages"
+import RemoveBackground from "./pages/RemoveBackground"
+import RemoveObject from "./pages/RemoveObject"
+import ReviewResume from "./pages/ReviewResume"
+import { useAuth } from "@clerk/clerk-react"
+import { useEffect } from "react"
+import {Toaster} from "react-hot-toast"
+
+
+
+function App() {
+
+  
+
+  return (
+    <>
+    <Toaster/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="Blog-title" element={<BlogTitle/>} />
+          <Route path="generate-images" element={<GenerateImages/>} />
+          <Route path="remove-background" element={<RemoveBackground/>} />
+          <Route path="remove-objects" element={<RemoveObject/>} />
+          <Route path="review-resume" element={<ReviewResume/>} />
+          <Route path="community" element={<Community />} />  
+
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
